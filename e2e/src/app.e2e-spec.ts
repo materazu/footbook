2f6ed7ed6431e1c943cd7fac0250b23a3ce6c0f0
Julien Moulin <julien@blueanchor.io>
2019-10-13 18:33:27 +0200

    Cover the app with e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -18,6 +18,67 @@ describe('workspace-project App', () => {
     expect(page.getSubTitleText()).toEqual('Discover champions.');
   });
 
+  it('should display search field with placeholder', () => {
+    page.navigateTo();
+    expect(page.getSearchFieldPlaceholder()).toEqual('Search by league');
+  });
+
+  it('should display suggestions when user click on search field', () => {
+    page.navigateTo();
+    page.clickOnSearchField();
+    expect(page.getSuggestionContainerResults()).toBeGreaterThan(0);
+  });
+
+  it('should filter results on suggestions when user type', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    expect(page.getFirstSuggestionResult()).toContain('French Ligue 2');
+  });
+
+  it('should displays all teams of a league when user click on result', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    page.selectLeagueInResult();
+    expect(page.getCardResults()).toEqual(18);
+  });
+
+  it('should displays team information on the card', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    page.selectLeagueInResult();
+    expect(page.getFirstTeamName()).toEqual('Ajaccio');
+    expect(page.getFirstTeamPicture()).toEqual('https://www.thesportsdb.com/images/media/team/badge/qpxvwy1473505505.png');
+  });
+
+  it('should go to the team page when user click on a team', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    page.selectLeagueInResult();
+    page.selectTeamInResult();
+    expect(page.getCurrentUrl()).toEqual('http://localhost:4200/team/133702');
+  });
+
+  it('should displays all members of the selected team', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    page.selectLeagueInResult();
+    page.selectTeamInResult();
+    expect(page.getCardResults()).toEqual(8);
+  });
+
+  it('should displays all informations on team member card', () => {
+    page.navigateTo();
+    page.sendSearchInSearchField();
+    page.selectLeagueInResult();
+    page.selectTeamInResult();
+    expect(page.getFirstMemberName()).toEqual(`Ali M'madi`);
+    expect(page.getFirstMemberPosition()).toEqual('Midfielder');
+    expect(page.getFirstMemberBirthDate()).toEqual('Birth date: 1990/04/21');
+    expect(page.getFirstMemberPrice()).toEqual('Price: Signed');
+    expect(page.getMemberPicture(1)).toEqual('https://www.alliancerehabmed.com/wp-content/uploads/icon-avatar-default.png');
+    expect(page.getMemberPicture(3)).toEqual('https://www.thesportsdb.com/images/media/player/cutout/8430842.png');
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -12,4 +12,72 @@ export class AppPage {
   getSubTitleText() {
     return element(by.css('app-root .subtitle')).getText() as Promise<string>;
   }
+
+  getSearchFieldPlaceholder() {
+    return element(by.css('.input-container input')).getAttribute('placeholder') as Promise<string>;
+  }
+
+  clickOnSearchField() {
+    const inputElement = element(by.css('.input-container input'));
+    inputElement.click();
+  }
+
+  getSuggestionContainerResults() {
+    return element.all(by.css('.suggestions-container ul li')).count() as Promise<number>;
+  }
+
+  sendSearchInSearchField() {
+    const inputElement = element(by.css('.input-container input'));
+    inputElement.sendKeys('ligue');
+  }
+
+  getFirstSuggestionResult() {
+    return element(by.css('.suggestions-container ul li:first-child')).getText() as Promise<string>;
+  }
+
+  selectLeagueInResult() {
+    const leagueResult = element(by.css('.suggestions-container ul li:first-child'));
+    leagueResult.click();
+  }
+
+  getCardResults() {
+    return element.all(by.css('.card')).count() as Promise<number>;
+  }
+
+  getFirstTeamName() {
+    return element(by.css('.card:first-child .team-name')).getText() as Promise<string>;
+  }
+
+  getFirstTeamPicture() {
+    return element(by.css('.card:first-child .image img')).getAttribute('src') as Promise<string>;
+  }
+
+  selectTeamInResult() {
+    const teamElement = element(by.css('.card:first-child'));
+    teamElement.click();
+  }
+
+  getCurrentUrl() {
+    return browser.getCurrentUrl() as Promise<string>;
+  }
+
+  getFirstMemberName() {
+    return element(by.css('.card:first-child .member-name')).getText() as Promise<string>;
+  }
+
+  getFirstMemberPosition() {
+    return element(by.css('.card:first-child .member-position')).getText() as Promise<string>;
+  }
+
+  getFirstMemberBirthDate() {
+    return element(by.css('.card:first-child .member-birth-date')).getText() as Promise<string>;
+  }
+
+  getFirstMemberPrice() {
+    return element(by.css('.card:first-child .member-price')).getText() as Promise<string>;
+  }
+
+  getMemberPicture(child) {
+    return element(by.css(`.column:nth-child(${child}) .image img`)).getAttribute('src') as Promise<string>;
+  }
 }
